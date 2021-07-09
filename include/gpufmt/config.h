bbb43e47722db15d01c26e74c0f05635945d3147
amc522 <1531573+amc522@users.noreply.github.com>
2021-07-08 23:23:36 -0700

    Adding support for more span libraries if not on c++20

--- include/gpufmt/config.h
+++ include/gpufmt/config.h
@@ -11,34 +11,47 @@
 
 #if defined(__cpp_lib_span) && GF_CPP_VERSION >= __cpp_lib_span
 #include <span>
-
-namespace gpufmt {
-    template<class T, size_t Extent = std::dynamic_extent>
-    using span = std::span<T, Extent>;
-}
-#else
+#define GF_HAS_STD_SPAN
+#elif __has_include("span.hpp")
 #include <span.hpp>
-
-namespace gpufmt {
-    using namespace tcb;
-}
+#elif __has_include("tcb/span.hpp")
+#include <tcb/span.hpp>
+#elif __has_include("nonstd/span.hpp")
+#include <nonstd/span.hpp>
 #endif
 
 // For glm::tvec3 used as Extent. If redefining gpufmt::Extent to another type, you can remove this include.
 #include <glm/vec3.hpp> 
 
 namespace gpufmt {
+#ifdef GF_HAS_STD_SPAN
+    inline constexpr auto span_dynamic_extent = std::dynamic_extent;
+
+    template<class T, size_t Extent = std::dynamic_extent>
+    using span = std::span<T, Extent>;
+#elif defined(TCB_SPAN_HPP_INCLUDED)
+    inline constexpr auto span_dynamic_extent = tcb::dynamic_extent;
+
+    template<class T, size_t Extent = tcb::dynamic_extent>
+    using span = tcp::span<T, Extent>;
+#elif defined(NONSTD_SPAN_HPP_INCLUDED)
+    inline constexpr auto span_dynamic_extent = nonstd::dynamic_extent;
+
+    template<class T, size_t Extent = nonstd::dynamic_extent>
+    using span = nonstd::span<T, Extent>;
+#endif
+
     using Extent = glm::tvec3<uint32_t>;
     using byte = std::byte;
 
     template<class T>
     gpufmt::span<const gpufmt::byte> asBytes(const T &value) {
-        return gpufmt::span<const gpufmt::byte>{reinterpret_cast<const std::byte*>(&value), sizeof(T)};
+        return gpufmt::span<const gpufmt::byte>{reinterpret_cast<const gpufmt::byte*>(&value), sizeof(T)};
     }
 
     template<class T>
     gpufmt::span<gpufmt::byte> asWritableBytes(T &value) {
-        return gpufmt::span<gpufmt::byte>{reinterpret_cast<std::byte*>(&value), sizeof(T)};
+        return gpufmt::span<gpufmt::byte>{reinterpret_cast<gpufmt::byte*>(&value), sizeof(T)};
     }
 
     template<class T, size_t Size>
