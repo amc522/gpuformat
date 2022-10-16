bb220fe86bd919515f61493f19e6049557132191
amc522 <1531573+amc522@users.noreply.github.com>
2022-10-16 14:11:35 -0700

    The double sample type was incorrectly being included in SampleVariant when GF_EXCLUDE_64BIT_FORMATS was defined.

--- include/gpufmt/internal/sample_variant.h
+++ include/gpufmt/internal/sample_variant.h
@@ -8,9 +8,9 @@
 #include <variant>
 
 namespace gpufmt {
-    using SampleVariant = std::variant<glm::vec4, glm::i32vec4, glm::u32vec4, glm::dvec4
+    using SampleVariant = std::variant<glm::vec4, glm::i32vec4, glm::u32vec4
 #ifndef GF_EXCLUDE_64BIT_FORMATS
-        , glm::i64vec4, glm::u64vec4
+        , glm::dvec4, glm::i64vec4, glm::u64vec4
 #endif
     >;
 }
